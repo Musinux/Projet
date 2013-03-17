ed879bb1e3cc78c5951b866f0c1d8445fba414d1
Louis Cherel <cherel.louis@gmail.com>
2013-03-17 16:02:41 +0100

    voilà

---
+++ generation.h
@@ -0,0 +1,10 @@
+#ifndef GENERATION_H__
+#define GENERATION_H__
+#define TAI 8
+
+void calcIndice(int*,int**,int*,int*,int,int);
+void genGrille(int**);
+void choixGrille(int**,int);
+void mask(int**,int);
+
+#endif // GENERATION_H__
