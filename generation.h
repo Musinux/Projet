2f70ea0ff8ecac69d49db9a6537454bc630bda2a
Musinux <cherel.louis@gmail.com>
2013-03-20 16:39:19 +0100

    Update generation.h

--- generation.h
+++ generation.h
@@ -1,7 +1,7 @@
 #ifndef GENERATION_H__
 #define GENERATION_H__
+#include "affichage.h"
 #define TAI 8
-
 void calcIndice(int*,int**,int*,int*,int,int);
 void genGrille(int**);
 void choixGrille(int**,int);
@@ -9,5 +9,6 @@ void choixMasque(int**,int);
 void initgrille_jeu(int **,int **, int **);
 int** creeTab();
 void supprTab(int **tab);
-
+coords* estValide(int **,coords *);
+void checkErreurs(int**, coords*);
 #endif // GENERATION_H__
