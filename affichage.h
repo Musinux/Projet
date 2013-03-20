17423eb2769de7da5b762a69ec7d56249ef1bb12
Musinux <cherel.louis@gmail.com>
2013-03-20 14:31:08 +0100

    Update affichage.h

--- affichage.h
+++ affichage.h
@@ -12,11 +12,12 @@ typedef struct coords{
     int x;
     int y;
     e_etat etat;
+    struct coords* prec;
 }coords;
-
 
 
 void deplJoueur(int**,int**,coords*);
 void affJoueur(int**,int**,coords*);
+void initCoords(coords*,coords*);
 
 #endif // AFFICHAGE_H__
